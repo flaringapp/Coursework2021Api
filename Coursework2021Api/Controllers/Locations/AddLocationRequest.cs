a3d76f3896cf639a1355a06102737a6549222137
Mallivance <ashpek01@gmail.com>
2021-04-30 01:25:52 +0300

    Cleanup code

--- Coursework2021Api/Controllers/Locations/AddLocationRequest.cs
+++ Coursework2021Api/Controllers/Locations/AddLocationRequest.cs
@@ -3,15 +3,10 @@ namespace Coursework2021Api.Controllers.Locations
     public class AddLocationRequest
     {
         public string Name { get; set; }
-
         public string? Description { get; set; }
-
         public float Lat { get; set; }
-
         public float Lon { get; set; }
-
         public string? Address { get; set; }
-
         public float Area { get; set; }
     }
 }
--- Coursework2021Api/Controllers/Locations/EditLocationRequest.cs
+++ Coursework2021Api/Controllers/Locations/EditLocationRequest.cs
@@ -3,17 +3,11 @@ namespace Coursework2021Api.Controllers.Locations
     public class EditLocationRequest
     {
         public string Id { get; set; }
-
         public string Name { get; set; }
-
         public string? Description { get; set; }
-
         public float Lat { get; set; }
-
         public float Lon { get; set; }
-
         public string? Address { get; set; }
-
         public float Area { get; set; }
     }
 }
