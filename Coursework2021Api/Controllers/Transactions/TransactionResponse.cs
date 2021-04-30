177f5e22b6986610a6ad4e78fcc75948a915f39f
Mallivance <ashpek01@gmail.com>
2021-04-30 12:26:53 +0300

    Make transaction fields nullable

--- Coursework2021Api/Controllers/Transactions/TransactionResponse.cs
+++ Coursework2021Api/Controllers/Transactions/TransactionResponse.cs
@@ -5,14 +5,14 @@ namespace Coursework2021Api.Controllers.Transactions
     public class TransactionResponse
     {
         public string Id { get; set; }
-        public string RentalId { get; set; }
-        public string UserId { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
-        public string RoomId { get; set; }
-        public string RoomName { get; set; }
-        public string RoomType { get; set; }
-        public string ManagerId { get; set; }
+        public string? RentalId { get; set; }
+        public string? UserId { get; set; }
+        public string? UserFirstName { get; set; }
+        public string? UserLastName { get; set; }
+        public string? RoomId { get; set; }
+        public string? RoomName { get; set; }
+        public string? RoomType { get; set; }
+        public string? ManagerId { get; set; }
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
         public int Amount { get; set; }
