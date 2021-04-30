022d270e80119ec566c2fe9c4228e2e0cfbc0e6b
Mallivance <ashpek01@gmail.com>
2021-04-30 03:23:00 +0300

    Add rentals controller

---
+++ Coursework2021Api/Controllers/Rentals/RentalResponse.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Coursework2021Api.Controllers.Rentals
+{
+    public class RentalResponse
+    {
+        public string Id { get; set; }
+        public string RoomId { get; set; }
+        public string RoomName { get; set; }
+        public int RoomPrice { get; set; }
+        public string UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+    }
+}
